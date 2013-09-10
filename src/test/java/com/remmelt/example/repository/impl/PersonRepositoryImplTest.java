bea1e2218a6a1041c781785602eae350f180632b
Daan Debie <daan.debie@info.nl>
2013-09-10 11:05:49 +0200

    Fixed test

--- src/test/java/com/remmelt/example/repository/impl/PersonRepositoryImplTest.java
+++ src/test/java/com/remmelt/example/repository/impl/PersonRepositoryImplTest.java
@@ -1,24 +1,34 @@
 package com.remmelt.example.repository.impl;
 
 
+import com.external.database.DatabaseAccess;
 import org.junit.Assert;
 import org.junit.Test;
 
 import com.remmelt.example.model.Person;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
 
 /**
  * Exercise 1
  * Implement the test in this class using Mockito.
  */
+@RunWith(MockitoJUnitRunner.class)
 public class PersonRepositoryImplTest {
 
+    @InjectMocks
 	private PersonRepositoryImpl personRepository;
+
+    @Mock
+    private DatabaseAccess databaseAccess;
 
 	@Test
 	public void testThatWhenSavePersonIsCalledItCallsDatabaseAccessSavePerson() {
 		personRepository.savePerson(new Person(1, "name", "email"));
-
-		Assert.fail();
+        Mockito.verify(databaseAccess).savePerson(Mockito.any(Person.class));
 	}
 
 }
